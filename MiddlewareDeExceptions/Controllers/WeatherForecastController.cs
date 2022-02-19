420c4003e0bebc2f25dc2c219a668b9292d8abeb
unknown <imbrices@gmail.com>
2022-02-18 21:32:42 -0300

    fc

---
+++ MiddlewareDeExceptions/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareDeExceptions.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                int cero = 0;
+                double div = 1 / cero;
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        
+        [HttpGet]
+        [Route("OtroGet")]
+        public async Task<IActionResult> OtroGet()
+        {
+            // Sin try catch
+            int cero = 0;
+            double div = 1 / cero;
+            return Ok();           
+        }
+    }
+}
---
+++ MiddlewareDeExceptions/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using MiddlewareDeExceptions.Middlewares.Entities;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MiddlewareDeExceptions.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+       
+        public ExceptionMiddleware(RequestDelegate next)
+        {      
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {              
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "El middleware responde : " + exception.Message
+            }.ToString());
+        }
+    }
+}
